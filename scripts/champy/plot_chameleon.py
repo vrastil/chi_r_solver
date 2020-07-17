11882f49fee3cb0c28b63a7bf1a386d3d837e47e
vrastil <michalvrastil@seznam.cz>
2020-07-17 12:38:50 +0200

    plot star

---
+++ scripts/champy/plot_chameleon.py
@@ -0,0 +1,65 @@
+"""
+'plot_chameleon.py' modules serves for plotting results of simulations
+"""
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import animation
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.cm as cm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.ticker import FormatStrFormatter
+
+# color-blind
+CB_colors = ['#377eb8', '#ff7f00', '#4daf4a',
+            '#f781bf', '#a65628', '#984ea3',
+            '#999999', '#e41a1c', '#dede00']
+# newcmp = ListedColormap(CB_colors, name='ColorBlind')
+
+
+# default values
+matplotlib.rcParams['legend.numpoints'] = 1
+matplotlib.rcParams['lines.linewidth'] = 4.0
+matplotlib.rcParams['lines.markersize'] = 6.0
+# matplotlib.rcParams['axes.prop_cycle'] = cycler(color=CB_colors)
+matplotlib.rcParams['axes.labelsize'] = 30
+matplotlib.rcParams['xtick.labelsize'] = 20
+matplotlib.rcParams['ytick.labelsize'] = 20
+matplotlib.rcParams['legend.fontsize'] = 25
+matplotlib.rcParams['font.size'] = 25
+
+dflt_suptitle_size = 25
+dflt_fig_size = (14, 9)
+dflt_fig_size_map = (14, 14)
+
+def plot_generic(data_all, **kwargs):
+    # extract arguments, set defaults
+    fig_size = kwargs.get('figsize', dflt_fig_size)
+    xscale = kwargs.get('xscale', 'log')
+    yscale = kwargs.get('yscale', 'log')
+    show = kwargs.get('show', True)
+
+    fig = plt.figure(figsize=fig_size)
+    ax = plt.gca()
+
+    # plot all data
+    for label, data in data_all.items():
+        x, y = data
+        ax.plot(x, y, label=label)
+
+    # set scales
+    ax.set_yscale(xscale)
+    ax.set_xscale(yscale)
+
+    # set limits
+    ax.set_xlim(xmax=10)
+
+    # legend
+    plt.legend()
+
+    # save, show, close
+    if show:
+        plt.show()
---
+++ scripts/champy/sim_chameleon.py
@@ -0,0 +1,114 @@
+"""
+'sim_chameleon.py' modules serves for running and sorting simulations
+"""
+
+import subprocess
+import os
+import sys
+import time
+
+import numpy as np
+
+from .plot_chameleon import plot_generic
+
+
+class Simulation(object):
+    def __init__(self, run_cmd, **kwargs):
+        self.run_cmd = run_cmd
+        self.sim_kwargs = kwargs
+
+    def set_kwargs(self, **kwargs):
+        for arg, val in kwargs.items():
+            self.sim_kwargs[arg] = val
+
+    def set_run_cmd(self, run_cmd):
+        self.run_cmd = run_cmd
+    
+    def run(self, stdout=subprocess.PIPE):
+        cmd = self.run_cmd
+        for arg, val in self.sim_kwargs.items():
+            cmd += f" --{arg}={val}"
+        if stdout is not None:
+            print(f"Running command '{cmd}' from {os.getcwd()}")
+        process  = subprocess.Popen(cmd, shell=True, stdout=stdout)
+        if stdout is not None:
+            print(process.stdout.read().decode('utf-8'))
+
+    def get_out_dir(self, key="out_dir"):
+        return self.sim_kwargs[key]
+
+    def get_data(self, a_file):
+        a_file = os.path.join(self.get_out_dir(), a_file)
+        return np.loadtxt(a_file).transpose()
+
+def get_all_kwargs(kwargs_sims, all_kwargs=None):
+    if all_kwargs is None:
+        all_kwargs = []
+
+    __kwargs_sim = kwargs_sims.copy()
+    for key, val_list in kwargs_sims.items():
+        if isinstance(val_list, list):
+            for val in val_list:
+                __kwargs_sim[key] = val
+                get_all_kwargs(__kwargs_sim, all_kwargs)
+            break
+    else:
+        all_kwargs.append(__kwargs_sim)
+    return all_kwargs
+
+
+def run_many_sims(kwargs_dflt, kwargs_sims, stdout=subprocess.PIPE):
+    # set default arguments
+    sim = Simulation("../build/main.a", **kwargs_dflt)
+    
+    # get all combinations of parameters
+    all_kwargs = get_all_kwargs(kwargs_sims)
+    
+    # for every combination run simulation and save results
+    results_all = []
+    for i, kwargs_sim in enumerate(all_kwargs):
+        # print some info
+        print(f"Running simulation {i+1}/{len(all_kwargs)}")
+
+        # run simulation
+        sim.set_kwargs(**kwargs_sim)
+        sim.run(stdout=stdout)
+
+        # wait to finish io
+        time.sleep(0.5)
+        
+        # save results
+        results = {
+            'params' : kwargs_sim,
+            'potential' : sim.get_data('potential.dat'),
+            'forces' : sim.get_data('forces.dat'),
+        }
+        
+        results_all.append(results)
+    return results_all
+
+def find_simulations(results_all, **kwargs):
+    results = []
+    for sim in results_all:
+        for key, val in kwargs.items():
+            if sim['params'][key] != val:
+                break
+        else:
+            results.append(sim)
+    return results
+
+def plot_simulations(results_all, p_type='potential', label_by='Ys', data_col=1, **kwargs):
+    # filter
+    sims_to_plot = find_simulations(results_all, **kwargs)
+
+    # extract data, set labels
+    data_all = {}
+    for sim in sims_to_plot:
+        data = sim[p_type][0], sim[p_type][data_col]
+        if isinstance(label_by, str):
+            label = f"{label_by} = {sim['params'][label_by]}"
+
+        data_all[label] = data
+
+    # plot
+    plot_generic(data_all)
