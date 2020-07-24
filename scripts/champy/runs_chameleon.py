d94ad165af87840ca1e7368bc40d5d7457d5e469
vrastil <vrastil@fzu.cz>
2020-07-24 14:16:36 +0200

    plot forces, eff pot

--- scripts/champy/runs_chameleon.py
+++ scripts/champy/runs_chameleon.py
@@ -11,7 +11,8 @@ kwargs_dflt = {
     "step": 0.01,
 }
 
-def run_plot_star_pot(parallel=True):
+
+def run_plot_star_pot(out_file='starlike.png', parallel=True):
     print(f"Running STAR-like simulation (potential)")
 
     kwargs_sims = {
@@ -30,14 +31,13 @@ def run_plot_star_pot(parallel=True):
     for sim in results_all:
         data = sim['potential'][0], sim['potential'][2]
         Ys = int(np.log10(sim['params']['Ys']))
-        label = f"$\Phi_s = 10^{{{Ys}}}$"
+        label = f"$\Phi_{{scr}} = 10^{{{Ys}}}$"
         data_all[label] = data
 
-
     plot_generic(data_all, ymin=1E-16, ymax=1E-13, xlabel=u'$r/R_s$', ylabel=u'$\chi$',
-                 out_file='starlike.png')
+                 out_file=out_file)
 
-def run_plot_star_for(parallel=True):
+def run_plot_star_for(out_file='starlike_forces.png', parallel=True):
     print(f"Running STAR-like simulation (forces)")
 
     kwargs_sims = {
@@ -57,16 +57,72 @@ def run_plot_star_for(parallel=True):
     for sim in results_all:
         data = sim['forces'][0], sim['forces'][1]
         R_eq = sim['params']['R_eq']
-        label = f"$R_{{eq}} = {R_eq}$"
+        label = f"$R_{{eq}} = {R_eq} R_s$"
         data_all[label] = data
 
 
     plot_generic(data_all, yscale='linear', ymin=0, ymax=1.05, xlabel=u'$r/R_s$',
-                 ylabel=u'$F_\chi/(2\\beta^2F_N)$', out_file='starlike_forces.png')
+                 ylabel=u'$F_\chi/(2\\beta^2F_N)$', out_file=out_file)
+
+
+def run_plot_nfw_for(out_file='nfwlike_forces.png', parallel=True):
+    print(f"Running NFW-like simulation (forces)")
+
+    kwargs_sims = {
+        "mod" : 1,
+        "Omega_m" : 1,
+        "c": 16,
+        "M200_sun": 1E-5,
+        "R_eq" : [-1, 0.1, 0.5, 1, 10],
+        "Ys" : 0,
+    }
+
+    results_all = run_many_sims(kwargs_dflt, kwargs_sims, stdout=None, parallel=parallel)
+    print(f"Plot and save.")
+
+    # extract data, set labels
+    data_all = {}
+    for sim in results_all:
+        data = sim['forces'][0], sim['forces'][1]
+        R_eq = sim['params']['R_eq']
+        label = f"$R_{{eq}} = {R_eq} R_s$"
+        data_all[label] = data
+
+
+    plot_generic(data_all, yscale='linear', ymin=0, ymax=1.05, xmax=1E2, xlabel=u'$r/R_s$',
+                 ylabel=u'$F_\chi/(2\\beta^2F_N)$', out_file=out_file)
+
+def run_plot_nfw_pot_eff( out_file='nfwlike_pot_eff.png', parallel=True):
+    print(f"Running NFW-like simulation (forces)")
+
+    kwargs_sims = {
+    "c": 4,
+    "M200_sun": 1E2,
+    "R_eq" : [-1, -100, 1],
+    "Ys" : 0,
+    }
+
+    results_all = run_many_sims(kwargs_dflt, kwargs_sims, stdout=None, parallel=parallel)
+    print(f"Plot and save.")
+
+    # extract data, set labels
+    data_all = {}
+    for sim in results_all:
+        data = sim['potential'][0], sim['potential'][3]
+        R_eq = sim['params']['R_eq']
+        label = f"$R_{{eq}} = {R_eq} R_s$"
+        data_all[label] = data
+
+
+    plot_generic(data_all, yscale='linear', ymin=0, xmin=1E-1, xmax=1E2, xlabel=u'$r/R_s$',
+                 ylabel=u'$\Phi_{scr,eff}/\Phi_{scr}$', out_file=out_file)
 
 def main():
-    run_plot_star_pot()
-    run_plot_star_for()
+    run_plot_star_pot(out_file='starlike.png')
+    run_plot_star_for(out_file='starlike_forces.png')
+    run_plot_nfw_for(out_file='nfwlike_forces.png')
+    run_plot_nfw_pot_eff(out_file='nfwlike_pot_eff.png')
+
 
 if __name__ == "__main__":
     main()
